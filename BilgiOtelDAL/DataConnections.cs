42161e31266b164b521075feb6af767114d8c0af
muhammedfener <muhammedfener97@gmail.com>
2022-11-23 19:57:25 +0300

    .

---
+++ BilgiOtelDAL/DataConnections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgiOtelDAL
+{
+    public class DataConnections
+    {
+        public static string MsSQLConnectionString
+        {
+            get { return "Server=.;Database=MyBilgiHotel;Trusted_Connection=True;"; }
+            set { MsSQLConnectionString = value; }
+        }
+    }
+
+}
