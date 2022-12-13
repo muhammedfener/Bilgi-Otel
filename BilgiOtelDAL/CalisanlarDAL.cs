229da14222013ca5960a82b42ccf6e8dcd5e0b44
muhammedfener <muhammedfener97@gmail.com>
2022-12-13 12:19:09 +0300

    Hata Düzeltmeleri.

--- BilgiOtelDAL/CalisanlarDAL.cs
+++ BilgiOtelDAL/CalisanlarDAL.cs
@@ -5,7 +5,6 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BilgiOtel;
 using Entity;
 namespace BilgiOtelDAL
 {
--- BilgiOtelDAL/KampanyalarDAL.cs
+++ BilgiOtelDAL/KampanyalarDAL.cs
@@ -1,5 +1,4 @@
-﻿using BilgiOtel;
-using Entity;
+﻿using Entity;
 using System;
 using System.Collections.Generic;
 using System.Data;
